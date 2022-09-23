7a9a89b43b28a51b429dbbf8c89a627308a6cd7c
hank <hank@henrygd.me>
2022-09-23 13:14:44 -0500

    add types

---
+++ index.ts
@@ -0,0 +1,36 @@
+export interface SidePanelOptions {
+	/** target container (where it's mounted and listens for touch) */
+	target: HTMLElement
+	/** panel content (will be removed and added in the panel) */
+	content?: HTMLElement
+	/** width of panel */
+	width?: number
+	/** open / close animation time */
+	duration?: number
+	/** fixed to screen */
+	fixed?: boolean
+	/** open on left */
+	left?: boolean
+	/** whether to open on touch drag */
+	dragOpen?: boolean
+	/** prevent HTML scrolling when fixed */
+	preventScroll?: boolean
+	/** runs when the menu is opened (as soon as it's visible) */
+	onShow?(container?: HTMLElement): void
+	/** runs when the menu is closed (as soon as it's hidden) */
+	onHide?(): void
+}
+
+export interface SidePanelMenu extends SidePanelOptions {
+	/** shows menu */
+	show(): void
+	/** hides menu */
+	hide(): void
+	/** destroys menu */
+	$destroy(): void
+}
+
+/** Initializes SidePanel instance */
+declare function SidePanel(options: SidePanelOptions): SidePanelMenu
+
+export default SidePanel
